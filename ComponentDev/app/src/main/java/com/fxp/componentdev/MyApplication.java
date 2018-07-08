1013f13e3c15134b522a33ea8a05ff930dcf9086
fangxiaopeng <850899969@qq.com>
2018-07-08 11:36:23 +0800

    提交MyApplication.java

---
+++ ComponentDev/app/src/main/java/com/fxp/componentdev/MyApplication.java
@@ -0,0 +1,35 @@
+package com.fxp.componentdev;
+
+import com.fxp.module_common.base.BaseApplication;
+
+/**
+ * Title:       MyApplication
+ * <p>
+ * Package:     com.fxp.componentdev
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/7/8 上午11:27
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/7/8    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class MyApplication extends BaseApplication {
+
+    private final static String TAG = MyApplication.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
