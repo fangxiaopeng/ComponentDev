88f92be1b0e314d130093dcd0fe9c4126668c059
fangxiaopeng <850899969@qq.com>
2018-07-08 11:35:43 +0800

    将MainActivity移到activity目录下

---
+++ ComponentDev/app/src/main/java/com/fxp/componentdev/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.fxp.componentdev.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fxp.componentdev.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
