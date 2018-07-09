bc959081a2f3d18a6c5672e82a5cd043c424eb2d
fangxiaopeng <850899969@qq.com>
2018-07-09 16:55:23 +0800

    添加跳转其他module示例

--- ComponentDev/app/src/main/java/com/fxp/componentdev/activity/MainActivity.java
+++ ComponentDev/app/src/main/java/com/fxp/componentdev/activity/MainActivity.java
@@ -1,18 +1,48 @@
 package com.fxp.componentdev.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.fxp.componentdev.R;
+import com.fxp.module_common.utils.ContextUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer.schedule(timerTask, 3000, 1000);
+
     }
 
+    private Timer timer = new Timer();
+    private TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            mHandler.sendEmptyMessage(0);
+        }
+    };
 
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            Intent intent = new Intent("com.fxp.module_homepage.activity.MainActivity");
+            ContextUtils.getContext().startActivity(intent);
+
+            timer.cancel();
+        }
+    };
 
 }
