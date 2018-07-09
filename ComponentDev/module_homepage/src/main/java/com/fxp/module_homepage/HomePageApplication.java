a67ac661600529b772edee5fe7bfc2573a45bbe5
fangxiaopeng <850899969@qq.com>
2018-07-09 16:02:23 +0800

    update HomePageApplication.java

---
+++ ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/HomePageApplication.java
@@ -0,0 +1,35 @@
+package com.fxp.module_homepage;
+
+import com.fxp.module_common.base.BaseApplication;
+
+/**
+ * Title:       HomePageApplication
+ * <p>
+ * Package:     com.fxp.module_homepage
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/7/8 上午11:57
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/7/8    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class HomePageApplication extends BaseApplication{
+
+    private final static String TAG = HomePageApplication.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
