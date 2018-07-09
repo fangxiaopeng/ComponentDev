515956efe51db7b46583a69c61c3d70eac2da4ff
fangxiaopeng <850899969@qq.com>
2018-07-09 16:01:44 +0800

    提交MainActivity

---
+++ ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fxp.module_homepage.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fxp.module_homepage.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
