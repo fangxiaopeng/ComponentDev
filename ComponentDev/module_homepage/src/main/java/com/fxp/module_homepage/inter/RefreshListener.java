974d8b3eec4345fac07866491be8637913923e6a
fangxiaopeng <850899969@qq.com>
2018-07-10 09:56:41 +0800

    提交RefreshListener.java

---
+++ ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/inter/RefreshListener.java
@@ -0,0 +1,27 @@
+package com.fxp.module_homepage.inter;
+
+/**
+ * Title:       RefreshListener
+ * <p>
+ * Package:     com.fxp.module_homepage.inter
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/7/9 下午6:01
+ * <p>
+ * Description: 刷新接口
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/7/9    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public interface RefreshListener {
+
+    void refresh();
+
+}
