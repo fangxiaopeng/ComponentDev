98294c97407ff4ce133da87f6a88e54b8e021862
fangxiaopeng <850899969@qq.com>
2018-07-10 09:59:44 +0800

    update MainActivity.java

--- ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/activity/MainActivity.java
+++ ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/activity/MainActivity.java
@@ -1,15 +1,36 @@
 package com.fxp.module_homepage.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
+import com.fxp.module_common.utils.ContextUtils;
 import com.fxp.module_homepage.R;
+import com.fxp.module_homepage.controller.HomePageMainController;
 
+/**
+ * 个人主页
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private Context context;
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.homepage_activity_main);
+
+        context = ContextUtils.getContext();
+
+        view = LayoutInflater.from(this).inflate(R.layout.homepage_activity_main,null);
+
+        new HomePageMainController(context, view);
     }
 }
