488cb87ccc888da2612489fff19c988ca35659e1
fangxiaopeng <850899969@qq.com>
2018-07-13 09:35:24 +0800

    提交Constants.java

---
+++ ComponentDev/module_common/src/main/java/com/fxp/module_common/utils/Constants.java
@@ -0,0 +1,31 @@
+package com.fxp.module_common.utils;
+
+/**
+ * Title:       Constants
+ * <p>
+ * Package:     com.fxp.module_common.utils
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/7/12 下午3:55
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/7/12    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class Constants {
+
+    /************************************************ 个人主页配置 ************************************************/
+    /**
+     * 个人信息
+     */
+    public final static String PATH_HOMEPAGE_UERRINFO = "homepage/api/userInfo.json";
+
+}
