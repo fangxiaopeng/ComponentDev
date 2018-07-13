152de0071375ca5acaacfeeaacb84578777e049b
fangxiaopeng <850899969@qq.com>
2018-07-13 11:52:09 +0800

    提交个人主页顶部导航栏适配器

---
+++ ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/adapter/HomePageNavAdapter.java
@@ -0,0 +1,86 @@
+package com.fxp.module_homepage.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fxp.module_common.utils.DensityUtils;
+import com.fxp.module_homepage.R;
+import com.fxp.module_homepage.model.UserInfoBean;
+
+import java.util.List;
+
+/**
+ * Title:       HomePageNavAdapter
+ * <p>
+ * Package:     com.fxp.module_homepage.adapter
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/7/12 下午8:30
+ * <p>
+ * Description: 个人主页-顶部视图-导航栏适配器
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/7/12    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class HomePageNavAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private final static String TAG = HomePageNavAdapter.class.getSimpleName();
+
+    private Context context;
+
+    private List<UserInfoBean.NavBean> datas;
+
+    private LayoutInflater layoutInflater;
+
+    public HomePageNavAdapter(Context context, List<UserInfoBean.NavBean> datas){
+        this.context = context;
+        this.datas = datas;
+
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return new NavHolder(layoutInflater.inflate(R.layout.item_homepage_nav_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof NavHolder){
+            ((NavHolder)holder).lable.setText(datas.get(position).getName());
+            // 动态调整item项宽度,（屏幕宽度 - 左右margin值）/ item项数目
+            ((NavHolder)holder).lable.setWidth((DensityUtils.getScreenWidth(context) - DensityUtils.dp2px(context, 10))/datas.size());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+    class NavHolder extends RecyclerView.ViewHolder{
+
+        TextView lable;
+
+        public NavHolder(View itemView){
+            super(itemView);
+
+            lable = itemView.findViewById(R.id.nav_lable);
+        }
+    }
+
+}
