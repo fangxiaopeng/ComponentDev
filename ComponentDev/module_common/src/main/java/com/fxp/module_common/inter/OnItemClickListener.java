a150d99b103b027663736ba6b218d44dc8ca8b5a
fangxiaopeng <850899969@qq.com>
2018-07-14 23:14:37 +0800

    提交OnItemClickListener.java

---
+++ ComponentDev/module_common/src/main/java/com/fxp/module_common/inter/OnItemClickListener.java
@@ -0,0 +1,27 @@
+package com.fxp.module_common.inter;
+
+/**
+ * Title:       OnItemClickListener
+ * <p>
+ * Package:     com.fxp.module_common.inter
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/7/14 下午3:46
+ * <p>
+ * Description: 列表Item项点击事件监听
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/7/14    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public interface OnItemClickListener {
+
+    void onItemClick(int position);
+
+}
