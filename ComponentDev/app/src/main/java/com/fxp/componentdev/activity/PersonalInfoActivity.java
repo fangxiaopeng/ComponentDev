0deaa41c974a4004124e3b8015b866c0d9d4cb51
fangxiaopeng <850899969@qq.com>
2018-07-20 19:19:33 +0800

    提交PersonalInfoActivity.java

---
+++ ComponentDev/app/src/main/java/com/fxp/componentdev/activity/PersonalInfoActivity.java
@@ -0,0 +1,15 @@
+package com.fxp.componentdev.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fxp.componentdev.R;
+
+public class PersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
