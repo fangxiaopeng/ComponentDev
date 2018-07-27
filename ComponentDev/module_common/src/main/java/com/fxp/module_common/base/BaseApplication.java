78ab7211216a55909cb64b407888fb3695f1e361
fangxiaopeng <850899969@qq.com>
2018-07-27 19:03:29 +0800

    添加ANR监控

--- ComponentDev/module_common/src/main/java/com/fxp/module_common/base/BaseApplication.java
+++ ComponentDev/module_common/src/main/java/com/fxp/module_common/base/BaseApplication.java
@@ -4,7 +4,9 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.fxp.module_common.AppBlockCanaryContext;
 import com.fxp.module_common.utils.ContextUtils;
+import com.github.moduth.blockcanary.BlockCanary;
 
 /**
  * Title:       BaseApplication
@@ -41,6 +43,7 @@ public class BaseApplication extends Application {
 
         ContextUtils.init(this);
 
+        BlockCanary.install(this, new AppBlockCanaryContext()).start();
     }
 
     /**  
