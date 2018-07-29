5a3a5fba77b0f64e6be982b6136fe094a3156370
fangxiaopeng <850899969@qq.com>
2018-07-29 23:32:15 +0800

    update PersonalInfoActivity.java

--- ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/activity/PersonalInfoActivity.java
+++ ComponentDev/module_homepage/src/main/java/com/fxp/module_homepage/activity/PersonalInfoActivity.java
@@ -11,5 +11,6 @@ public class PersonalInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+
     }
 }
