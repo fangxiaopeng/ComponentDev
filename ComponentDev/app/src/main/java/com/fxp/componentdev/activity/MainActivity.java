ee27cf06f61e86b46847e6809220dab8c8e6c5e8
fangxiaopeng <850899969@qq.com>
2018-08-02 11:00:16 +0800

    update MainActivity.java

--- ComponentDev/app/src/main/java/com/fxp/componentdev/activity/MainActivity.java
+++ ComponentDev/app/src/main/java/com/fxp/componentdev/activity/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cordova_fragment);
+        setContentView(R.layout.activity_main);
 
         timer.schedule(timerTask, 3000, 1000);
 
